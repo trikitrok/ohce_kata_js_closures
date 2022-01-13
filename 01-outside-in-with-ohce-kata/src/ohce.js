acd6dd61bcf9061160bcc1de98a4a383153a8c6f
trikitrok <trikitrok@gmail.com>
2022-01-13 11:32:59 +0100

    kata solution playing with closures

--- 01-outside-in-with-ohce-kata/src/ohce.js
+++ 01-outside-in-with-ohce-kata/src/ohce.js
@@ -1,3 +1,70 @@
-export default () => {
-  return false;
+export default (clock, userInput, display) => {
+    return {
+        run: (userName) => {
+            greetUser(userName);
+            
+            while(true) {
+                const word = getWord();
+
+                if(isStop(word)) {    
+                    sayBye(userName);
+                    return;
+                }
+                 
+                process(word);      
+            }
+            
+            function isStop(word) {
+                return word === "Stop!"
+            }
+            
+            function getWord() {
+                return userInput.get();
+            }
+        }
+    };
+  
+    function sayBye(userName) {
+        display.show(`Adios ${userName}`);
+    }
+    
+    function process(word) {
+        display.show(reversedWord());
+        
+        if(wordIsPalindrome()) {
+            celebratePalindrome();
+        }       
+        
+        function reversedWord() {
+            return word.split("").reverse().join("");
+        }
+  
+        function wordIsPalindrome() {
+            return word === reversedWord();
+        } 
+        
+        function celebratePalindrome() {
+            display.show("¡Bonita palabra!")
+        }
+    }
+  
+    function greetUser(userName) {
+        display.show(greetingMessage(clock.hour()));
+        
+        function greeting(hour) {
+            if(hour >= 6 && hour < 12) {
+                return "Buenos días";
+            }
+
+            if(hour >= 12 && hour < 20) {
+                return "Buenas tardes"
+            }
+
+            return "Buenas noches";
+        }
+
+        function greetingMessage(hour) {
+            return `¡${greeting(hour)} ${userName}!`;
+        }
+    }  
 }
--- 01-outside-in-with-ohce-kata/src/ohce.test.js
+++ 01-outside-in-with-ohce-kata/src/ohce.test.js
@@ -1,5 +1,104 @@
 import ohce from "./ohce";
 
-test("canary test", () => {
- expect(false).toEqual(true);
+describe("ohce", () => {
+    let clock;
+    let userInput
+    let display;
+    
+    beforeEach(() => {
+        clock = { hour: jest.fn() };
+        userInput = { get: jest.fn() };
+        display = { show: jest.fn() };
+    });
+
+    describe("stopping right after starting so it only greets the user", () => { 
+        beforeEach(() => {
+            userInput.get.mockReturnValueOnce("Stop!");
+        });
+        
+        test.each([6, 7, 11])("during the morning at %p", (hour) => {
+            clock.hour.mockReturnValue(hour);  
+            
+            ohce(clock, userInput, display).run("Pepe");
+            
+            expect(display.show.mock.calls).toEqual([
+                ["¡Buenos días Pepe!"], 
+                ["Adios Pepe"]
+            ]);
+            
+        });
+        
+        test.each([12, 14, 19])("during the afternoon at %p", (hour) => {
+            clock.hour.mockReturnValue(hour);  
+    
+            ohce(clock, userInput, display).run("Pepe");
+            
+            expect(display.show.mock.calls).toEqual([
+                ["¡Buenas tardes Pepe!"], 
+                ["Adios Pepe"]
+            ]);
+            
+        });
+        
+        test.each([20, 22, 0, 5])("during the night at %p", (hour) => {
+            clock.hour.mockReturnValue(hour);  
+    
+            ohce(clock, userInput, display).run("Pepe");
+            
+            expect(display.show.mock.calls).toEqual([
+                ["¡Buenas noches Pepe!"], 
+                ["Adios Pepe"]
+            ]);
+            
+        });
+    });
+    
+    describe("reversing a word", () => {
+        test.each([["hola", "aloh"], ["koko", "okok"]])("like %p", (word, reversedWord) => { 
+            clock.hour.mockReturnValue(7);
+            userInput.get.mockReturnValueOnce(word).mockReturnValueOnce("Stop!");        
+            
+            ohce(clock, userInput, display).run("Juan");
+            
+            expect(display.show.mock.calls).toEqual([
+                ["¡Buenos días Juan!"], 
+                [reversedWord],
+                ["Adios Juan"]
+            ]);
+        });
+    });
+    
+    describe("finding a palindrome", () => {
+        test.each(["oto", "ana"])("like %p", (palindrome) => { 
+            clock.hour.mockReturnValue(7);
+            userInput.get.mockReturnValueOnce(palindrome).mockReturnValueOnce( "Stop!");        
+            
+            ohce(clock, userInput, display).run("Juan");
+            
+            expect(display.show.mock.calls).toEqual([
+                ["¡Buenos días Juan!"], 
+                [palindrome],
+                ["¡Bonita palabra!"],
+                ["Adios Juan"]
+            ]);
+        });
+    });
+    
+    describe("real scenario", () => {
+        it("receiving several words", () => {
+            clock.hour.mockReturnValue(7);
+            userInput.get.mockReturnValueOnce("loko").mockReturnValueOnce("hannah").mockReturnValueOnce( "Stop!");        
+            
+            ohce(clock, userInput, display).run("Juan");
+            
+            expect(display.show.mock.calls).toEqual([
+                ["¡Buenos días Juan!"], 
+                ["okol"], 
+                ["hannah"],
+                ["¡Bonita palabra!"],
+                ["Adios Juan"]
+            ]);
+        });
+    });    
 });
+
